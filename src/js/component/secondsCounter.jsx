9e140e9d9e1f3d8903d3ce93c81d17d51e13a56b
sofi-rv <128938425+sofi-rv@users.noreply.github.com>
2023-07-28 03:13:14 +0000

    cambios

--- src/js/component/secondsCounter.jsx
+++ src/js/component/secondsCounter.jsx
@@ -4,68 +4,65 @@ const SecondsCounter = (props) => {
     const oneDigit = (tiempo) => {
         let segundos = tiempo
         let longitud = String(segundos).length
-        let ultimaPosicion = String(segundos)[longitud - 1]
-        return ultimaPosicion;
+        let primeraPosicion = String(segundos)[longitud - 1]
+        return primeraPosicion;
     }
     const secondDigit = (tiempo) => {
         let segundos = tiempo
         let longitud = String(segundos).length
-        let ultimaPosicion = String(segundos)[longitud - 2]
-        return ultimaPosicion;
+        let segundaPosicion = String(segundos)[longitud - 2]
+        return segundaPosicion;
     }
     const thirdDigit = (tiempo) => {
         let segundos = tiempo
         let longitud = String(segundos).length
-        let ultimaPosicion = String(segundos)[longitud - 3]
-        return ultimaPosicion;
+        let terceraPosicion = String(segundos)[longitud - 3]
+        return terceraPosicion;
     }
     const fourthDigit = (tiempo) => {
         let segundos = tiempo
         let longitud = String(segundos).length
-        let ultimaPosicion = String(segundos)[longitud - 3]
-        return ultimaPosicion;
+        let cuartaPosicion = String(segundos)[longitud - 4]
+        return cuartaPosicion;
     }
     const fifthDigit = (tiempo) => {
         let segundos = tiempo
         let longitud = String(segundos).length
-        let ultimaPosicion = String(segundos)[longitud - 3]
-        return ultimaPosicion;
+        let quintaPosicion = String(segundos)[longitud - 5]
+        return quintaPosicion;
     }
     const sixthDigit = (tiempo) => {
         let segundos = tiempo
         let longitud = String(segundos).length
-        let ultimaPosicion = String(segundos)[longitud - 3]
-        return ultimaPosicion;
+        let sextaPosicion = String(segundos)[longitud - 6]
+        return sextaPosicion;
     }
 
     return (
         <>
             <div className="container">
-                <div className="row bg bg-dark row-counter">
-                    <div className="col clock text-light text-center"><i class="far fa-clock fa-flip-horizontal"></i></div>
-                    <div className="col six text-light">
-                    <h1>{props.secondSix ? sixthDigit(props.secondSix) : "0"}</h1>
+                <div className="row mt-2 row-counter">
+                    <div className=" col text-light text-center border-1 border-start border-secondary border-end clock"><i class="far fa-clock fa-flip-horizontal"></i></div>
+                    <div className="col text-light text-center seconds border-1 border-start border-secondary border-end">
+                    <h1>{sixthDigit(props.secondSix) > 0 ? sixthDigit(props.secondSix) : "0"}</h1>
                     </div>
-                    <div className="col five text-light">
-                    <h1>{props.secondFive ? fifthDigit(props.secondFive) : "0"}</h1>
+                    <div className="col text-light text-center seconds border-1 border-start border-secondary border-end">
+                    <h1>{fifthDigit(props.secondFive) > 0 ? fifthDigit(props.secondFive) : "0"}</h1>
                     </div>
-                    <div className="col four text-light">
-                    <h1>{props.secondFour ? fourthDigit(props.secondFour) : "0"}</h1>
+                    <div className="col text-light text-center seconds border-1 border-start border-secondary border-end">
+                    <h1>{fourthDigit(props.secondFour) > 0 ? fourthDigit(props.secondFour) : "0"}</h1>
                     </div>
-                    <div className="col three text-light">
-                    <h1>{props.secondThree ? thirdDigit(props.secondThree) : "0"}</h1>
+                    <div className="col  text-light text-center seconds border-1 border-start border-secondary border-end">
+                    <h1>{thirdDigit(props.secondThree) > 0 ? thirdDigit(props.secondThree) : "0"}</h1>
                     </div>
-                    <div className="col two text-light">
-                        <h1>{props.secondTwo ? secondDigit(props.secondTwo) : "0"}</h1>
+                    <div className="col text-light text-center seconds border-1 border-start border-secondary border-end">
+                        <h1>{secondDigit(props.secondTwo) > 0 ? secondDigit(props.secondTwo) : "0"}</h1>
                     </div>
-                    <div className="col one text-light">
-                        <h1>{props.secondOne ? oneDigit(props.secondOne) : "0"}</h1>
+                    <div className="col text-light  text-center seconds border-1 border-start border-secondary border-end">
+                        <h1>{oneDigit(props.secondOne) > 0 ? oneDigit(props.secondOne) : "0"}</h1>
                     </div>
                 </div>
             </div>
-
-
-
 
         </>
 
